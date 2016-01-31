ee56b15b10265a23457f9032030fe0cca20acb5d
Pelle Jacobs <pelle.jacobs@novicap.com>
2016-01-31 17:18:20 +0100

    update french version

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 /*------------------
-.		MENU	
+.		MENU
 ------------------*/
 jQuery('#language').click(function(e) {
 	var path = location.origin + location.pathname;
 	var lang = document.getElementsByTagName("html")[0].getAttribute("lang");
 	if (lang == "en"){
-		location.href = path+"?lang=fr";
+		location.href = "/fr.html";
 	} else {
-		location.href = path+"?lang=en";
+		location.href = "/";
 	}
 });
 
 /*------------------
-.		TAB TRACKING	
+.		TAB TRACKING
 ------------------*/
 (function ($) {
 	// $('#myTab a').click(function (e) {
@@ -48,12 +48,12 @@ jQuery('#language').click(function(e) {
 
 
 /*------------------
-.		TEAM	
+.		TEAM
 ------------------*/
 (function ($) {
 	$('.se-member').hover(
 		function(){
-			// fade in 
+			// fade in
 			try {
 				_gaq.push(['_trackEvent', 'team', 'display caption', $(this).find(".caption h3").text(),, false]);
 
@@ -63,28 +63,7 @@ jQuery('#language').click(function(e) {
 			$(this).find('.caption').fadeIn(250);
 		},
 		function(){
-			$(this).find('.caption').fadeOut(250); 
+			$(this).find('.caption').fadeOut(250);
 		}
-		); 
+		);
 }(jQuery));
-
-
-/*------------------
-.		CONTACT FORM	
-------------------*/
-
-function successSent() {
-	var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var message = document.getElementById('message').value;
-
-	var payload = {
-		username: "Website",
-		icon_url: "http://senergie.academicsfordevelopment.be/wp-content/uploads/2015/02/Logo-Senergie-leeuw.png",
-		text: "_New website message:_ \n*Name:* "+name+"\n*Email:* "+email+"\n*Message:* "+message
-	}
-	console.log(payload);
-	var slackResponse = new XMLHttpRequest();
-	slackResponse.open("POST", "https://hooks.slack.com/services/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", true);
-	slackResponse.send(JSON.stringify(payload));
-}
